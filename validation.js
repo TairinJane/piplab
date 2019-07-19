e9000da20bbf0f13870f8f18a69e1f25cb90ffad
tairinjane <tairinjane@gmail.com>
2019-07-19 21:07:20 +0700

    add red border color for invalid input

--- validation.js
+++ validation.js
@@ -1,30 +1,43 @@
 function validate() {
     let x, y, R, text, valid;
 
-    x = document.getElementById("X").value;
-    y = document.getElementById("Y").value;
-    R = document.getElementById("R").value;
-    valid = false;
-    //alert(x+" "+y+" "+R);
+    x = document.getElementById("X");
+    y = document.getElementById("Y");
+    R = document.getElementById("R");
+    valid = true;
+    text = "Invalid input";
+    //alert(x.value + " " + y.value + " " + R.value + "\n" + (+x.value) + " " + (+y.value) + " " + (+R.value));
+    //alert(isNaN(x.value));
 
-    if (isNaN(x)) {
-        text = "Invalid X";
-    } else if (isNaN(y)) {
-        text = "Invalid Y";
-    } else if (isNaN(R) || R<=0) {
-        text = "Invalid R";
-    } else {
+    if (x.value === "" || +x.value !== +x.value) {
+        x.style.borderColor = "red";
+        //text = "Invalid X";
+        valid = false;
+    } else x.style.borderColor = "darkgrey";
+
+    if (y.value === "" || +y.value !== +y.value) {
+        y.style.borderColor = "red";
+        //text = "Invalid Y";
+        valid = false;
+    } else y.style.borderColor = "darkgrey";
+
+    if (R.value === "" || +R.value !== +R.value || R.value <= 0) {
+        R.style.borderColor = "red";
+        //text = "Invalid R";
+        valid = false;
+    } else R.style.borderColor = "darkgrey";
+
+    if (valid) {
         text = "All right!";
-        valid = true;
     }
     document.getElementById("text").innerHTML = text;
     //alert(text);
 
     if (valid) {
-        $.get("./script.php", {X: x, Y: y, R: R}, function (html) {
+        $.get("./script.php", {X: x.value, Y: y.value, R: R.value}, function (html) {
             //alert(html);
             document.getElementById("result").innerHTML = html;
-        } )
+        })
     }
 
     return false;
